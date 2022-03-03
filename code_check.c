8597b708e2e6b26f6d217219ec0dffa1244d9be8
StartaBafras <beyhankaradag00@gmail.com>
2022-03-03 17:06:25 +0300

    Kodun derlenip derlenmediğini kontrol eden kısım

---
+++ code_check.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "code_check.h"
+
+void move_and_compile(void)
+{
+    FILE *original = fopen("question.txt","r");
+    FILE *c_code = fopen("input.c","w");
+
+    char temp[50];
+
+    while (!feof(original))
+    {
+        memset(temp,0,50);
+
+        for(int i=0; i<50; i++)
+        {   
+            temp[i] = fgetc(original);
+            if(temp[i] == '\n' || temp[i] == -1)
+            {
+                if(temp[i] == -1) temp[i] = 0;
+                break;
+            }
+        }
+        
+        fprintf(c_code,"%s",temp);
+    }
+    fclose(c_code);
+    system("make input");
+}
