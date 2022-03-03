e0e855cd35b4e93b11f896a8a7b8bb3e88d81371
StartaBafras <beyhankaradag00@gmail.com>
2022-03-03 17:06:41 +0300

    prototipler

---
+++ code_check.h
@@ -0,0 +1,11 @@
+#ifndef __CODE_CHECk_H
+#define __CODE_CHECk_H
+
+/**
+ * @brief Verilen kodu txt dosyasından alıp C dosyasına dönüştürür ve derler.
+ *  Yeni dosyayı "q.c" şeklinde kayıt eder.
+ * @param void boş
+ */
+void move_and_compile(void);
+
+#endif
