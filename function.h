478f60a901c71bb018719b9df888dd65b8424ba6
StartaBafras <beyhankaradag00@gmail.com>
2022-03-07 19:22:22 +0300

    Fonksiyon yapıları oluşturuldu

---
+++ function.h
@@ -0,0 +1,28 @@
+#ifndef __FUNCTİON_H
+#define __FUNCTİON_H
+
+#include "loop.h"
+#include "variable.h"
+
+typedef struct function_s
+{
+    char name[20];
+    int start_end_line[2];
+    loop_s_pointer loops_inside;
+    struct function_s_pointer *function_inside;
+    int complexity;
+    int amount_call;
+    int call_line;
+    struct function_s *next;
+
+}function_s;
+
+typedef struct function_s_pointer
+{
+    function_s *function; 
+    function_s *next;
+
+}function_s_pointer;
+
+
+#endif
