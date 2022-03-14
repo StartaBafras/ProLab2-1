e6291b4a93fce5e0d76a303118068fcfff6e33f8
alpy <alperenklt24@gmail.com>
2022-03-14 14:15:43 +0300

    döngüleri bulan fonksiyonlar yazıldı

--- loop.h
+++ loop.h
@@ -3,7 +3,12 @@
 
 
 #include "variable.h"
-
+#define v_FOR 0x10
+#define V_WHILE 0x11
+#define V_WHILE_DO 0x16
+#define Size 150// satır ve sutun sayısı
+#define c_Size_s 10//small
+#define c_Size_l 30//large
 typedef struct loop_s
 {
     int id;
@@ -11,6 +16,8 @@ typedef struct loop_s
     int start_end_line[2];
     veriable_s_pointer dependent_variable;
     int complexity;
+    int id_var;
+    char condition[10];
     struct loop_s *next;
 
 }loop_s;
@@ -28,5 +35,24 @@ int add_loop(loop_s *data, loop_s *loops);
 
 
 int write_loop_data(loop_s *data,loop_s *loops);
+
+
+int find_for(char text[][Size], loop_s *l_root); // for yerini ve artış oranını bulur
+
+
+int find_line_end(int line_start, char text[][Size]); // iç içe for bulur
+
+int find_while(char text[][Size], loop_s *l_root);
+
+int find_do(char text[][Size], int end_while_line);
+
+void loop_and_variable(variable_s *v_root, loop_s *l_root);
+
+int and_variable(variable_s *v_root, int loop_start_line);
+
+int and_variable_case_2(variable_s *v_root, loop_s *l_root);
+
+int find_line_end(int line_start, char text[][Size]); // iç içe for bulur
+
 
 #endif
