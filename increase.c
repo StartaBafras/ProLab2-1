8099c455bf86f6ec4884d1076c23dfe94c6bebb6
StartaBafras <beyhankaradag00@gmail.com>
2022-03-14 11:43:51 +0300

    Artış ile ilgili bazı fonksiyonların prototipleri yazıldı

--- increase.c
+++ increase.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include "increase.h"
 
-#define Lineer 1
-#define Exponential 2
-#define Logaritmic 3
+
 
 /**
 * @brief Bir dizinin içinde isenen karakteri bulup 
@@ -15,19 +15,28 @@
 * @return (int) -1: Bulunamadı
 *
 */
-int find_char(char *message, char c,int size)
+int find_char(char *message, char c, int size)
 {
-    for(int i=0; i < size; i++)
+    if(message != NULL)
     {
+            for(int i=0; i < size; i++)
+        {
         if(message[i] == c) return i;
-    }
+        }
 
     return -1;
+    }
+
 }
 
-
-
-int find_increase(char *message,int size)
+/**
+ * @brief "i++" gibi arttırımları tespit etmeye çalışır
+ * @param message İgili satır
+ * @param size İlgili satırın boyutu
+ * @return -1: Bulunamadı 
+ * 
+ */
+int find_increase_case1(char *message, int size)
 {
     int error= 0;
 
@@ -36,7 +45,7 @@ int find_increase(char *message,int size)
     {
         if(message[error+1] == '+')
         {
-            return Lineer; // Lineer artış
+            return LINEER_POSITIVE; // Lineer artış
         }
     }
 
@@ -45,13 +54,134 @@ int find_increase(char *message,int size)
     {
         if(message[error+1] == '-')
         {
-            return Lineer; // Lineer artış
+            return LINEER_NEGATIVE; // Lineer azalış
         }
     }
 
     return -1;
 }
 
+
+int find_increase_case2(char *message, int size)
+{
+    int error = 0;
+    error = find_char(message,'=',size);
+    if(error != -1)
+    {
+        if(message[error-1] == '+')
+        {
+            return LINEER_POSITIVE;
+        }
+        else if(message[error-1] == '-')
+        {
+            return LINEER_NEGATIVE;
+        }
+        else if(message[error-1] == '*')
+        {
+            return EXPONENTIAL_POSITIVE;
+        }
+        else if(message[error-1] == '/')
+        {
+            return EXPONENTIAL_NEGATIVE;
+        }
+    }
+    
+    return -1;
+}
+
+/**
+ * @brief Başka değişkene bağlı olarak artan veya azalan değişkenleri analiz eder.
+ * @param message İlgili satırın tamamı
+ * @param size Satırı içeren dizinin uzunluğu
+ * 
+ */
+int find_complex_increase(char *message, int size)
+{
+    int error = 0;
+    int word = 1;
+
+    error = find_char(message,'=',size); // Eşittir olup olmadığının kontrolü
+
+    if(error == -1) return -1;
+    
+    for(int i=0; i<size; i++) if(message[i] == ' ') word++;
+
+    char *m_p[word];
+    sentence_divider(m_p,message,word);
+    
+    error = find_char(message,'(',size);
+
+    if(error == -1) return -1;
+
+
+
+
+
+}
+
+int calculata_compexity(char *d_m[],int word)
+{
+    int i=0;
+    for(;d_m[i] != '=';i++);
+
+
+
+}
+
+/**
+ * @brief Alınan mesajın içindeki virül karekterlerini boşluk olarak değiştirir.
+ * @param message Karakterleri içeren dizi.
+ * @param size Dizinin boyutu.
+ */
+int clear_line(char *message,int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        if(message[i] == ',')
+        {
+            message[i] = ' ';
+        }
+    }
+}
+int find_variables_by_name(char *message, int size)
+{
+    int word = 1;
+
+    for(int i=0; i<size; i++) if(message[i] == ' ') word++;
+
+
+}
+
+/**
+ * @brief Verilen diziyi boşluklara göre parçalayarak bir işaretçiye kayıt eder.
+ * @param message_p Verilerin tutulacağı char işaretçisi dizisi.
+ * @param message Parçalanacak cümle.
+ * @param word Cümledeki kelime sayısı.
+ */
+int sentence_divider(char *message_p[],char *message, int word)
+{
+    int error = 0;
+
+    message_p[0] = strtok(message," ");
+
+    for(int i=1; i<word;i++)
+    {
+        message_p[i] = strtok(NULL," ");
+        
+    }
+
+    //Dizileri temizleme işlemi
+    for(int i=0; i < word; i++)
+    {
+        error = find_char(message_p[i],'[',MAX_SENTENCE_LENGTH);
+        if(error != -1)
+        {
+            message_p[i][error] = '\0';
+        }
+    }
+
+    return 0;
+}
 /*
 int main(void)
 {
--- increase.h
+++ increase.h
@@ -1,10 +1,23 @@
 #ifndef __INCREASE_H
 #define __INCREASE_H
 
+#define LINEER_POSITIVE 1
+#define LINEER_NEGATIVE 2
+#define EXPONENTIAL_POSITIVE 3
+#define EXPONENTIAL_NEGATIVE 4
+#define LOGARITMIC_POSİTİVE 5
+#define LOGARITMIC_NEGATIVE 6
+
+#define MAX_SENTENCE_LENGTH 30
 
 int find_char(char *message, char c,int size);
 
-int find_increase(char *message,int size);
+int find_increase_case1(char *message,int size);
 
+int find_increase_case2(char *message, int size);
+
+int sentence_divider(char *message_p[],char *message, int word);
+
+int find_complex_increase(char *message, int size);
 
 #endif
