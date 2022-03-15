96f4abfc4ca06ed1edd88251505314fb3fff7111
alpy <alperenklt24@gmail.com>
2022-03-15 20:20:57 +0300

    Fonksiyon prototipleri eklendi

--- function.h
+++ function.h
@@ -26,4 +26,10 @@ typedef struct function_s_pointer
 
 loop_s* search_loop(int begin_line, loop_s *root);
 
+int add_loop_in_function(function_s *function_struct, int loop_begin_line, loop_s *root);
+
+int add_function(function_s *data, function_s *function_root);
+int write_function_data(function_s *data, function_s *function_root);
+
+int find_variables(char text[][Size], variable_s *root,function_s *f_root); // değişken türünü ismini yerini bulur
 #endif
