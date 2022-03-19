507af0686b2a212429e7a51ba2514ea0c65c1fa8
alpy <alperenklt24@gmail.com>
2022-03-19 23:28:28 +0300

    fonksiyon yorumu kaldırıldı

--- variable.c
+++ variable.c
@@ -8,82 +8,80 @@
  * @brief Değişkenlere ait verileri bağlı listede boş olduğu kabul edilen yere yazar.
  * @param data Verilerin kapsüllendiği veri paketinin adresi
  * @param variable_p İçi boş kabul edilen bağlı liste üyesinin adresi.
- */ 
+ */
 int write_variable_data(variable_s *data, variable_s *variable_p)
 {
     variable_p->id = data->id;
     strcpy(variable_p->kind, data->kind);
     variable_p->line = data->line;
-    strcpy(variable_p->name,data->name);
+    strcpy(variable_p->name, data->name);
     variable_p->increase_rate = NULL;
     variable_p->next = NULL;
-    
 }
 
 /**
  * @brief Tespit edilen değişkeni değişkenlerin toplandığı bağlı listeye ekler.
  * @param data Verilerin kapsüllendiği veri paketinin adresi.
  * @param variable_p Verileri işaret eden kök işaretçisi.
- * 
+ *
  */
 int add_variable_data(variable_s *data, variable_s *variable_p)
 {
-    if(variable_p->line == NULL)
+    if (variable_p->line == NULL)
     {
         data->id = 0;
-        write_variable_data(data,variable_p);
+        write_variable_data(data, variable_p);
         return 0;
     }
-     while (variable_p->next != NULL)
+    while (variable_p->next != NULL)
         variable_p = variable_p->next;
     data->id = variable_p->id + 1;
     variable_s *variable_p_new = malloc(sizeof(variable_s));
     variable_p->next = variable_p_new;
     write_variable_data(data, variable_p->next);
-
 }
 
 /**
- * @brief Değişkenler bağlı listesine kayıtlı olan bir değişkeni 
+ * @brief Değişkenler bağlı listesine kayıtlı olan bir değişkeni
  * bağlı listede arar ve adresini gönderir.
- * 
+ *
  * @param variable Değişkenin ismi.
  * @param variable_line Değişkenin bulunduğu satırç
  * @param root Değişkenleri tutan bağlı listenin kök adresi.
- * 
+ *
  * @return variable_s türünden adres.
  */
-variable_s* search_variable(char *variable,int varibale_line ,variable_s *root)
+variable_s *search_variable(char *variable, int varibale_line, variable_s *root)
 {
-    
-    while(1)
+
+    while (1)
     {
-        if( 0 == strcmp(variable,root->name) && varibale_line >= root->line)
+        if (0 == strcmp(variable, root->name) && varibale_line >= root->line)
         {
-          //  printf("%d\n",root->line);
+            //  printf("%d\n",root->line);
             return root;
         }
 
-        if(root->next != NULL) root = root->next;
-        else break;
+        if (root->next != NULL)
+            root = root->next;
+        else
+            break;
     }
 
     return NULL;
-
 }
 
-
 /**
- *@brief variable struct'ında türlere bakıp boyutunu ekler
+ *@brief Variable struct'ında türlere bakıp boyutunu ekler
  *@param v_root variable struct'ın root'unu alır
  */
-void find_size(variable_s *v_root,int start_line ,int  end_line)
+void find_size(variable_s *v_root, int start_line, int end_line)
 {
     char variables[c_Size_s][c_Size_s] = {"pointer", "int", "double", "char", "float", "long", "short", "matrix", "dizi"}; // türler
     char sizes[c_Size_s][c_Size_s] = {"8", "4", "8", "1", "4", "4", "2", "nn", "n"};
 
     // memset(v_root->size, NULL, c_Size_l);
-    if ((v_root != NULL)&&((start_line>v_root->line)||(end_line<v_root->line)))
+    if ((v_root != NULL) && ((start_line > v_root->line) || (end_line < v_root->line)))
     {
         for (int i = 0; i < 9; i++)
         {
@@ -101,22 +99,25 @@ void find_size(variable_s *v_root,int start_line ,int  end_line)
             }
         }
     }
-    //printf("%s --%s\n", v_root->size, v_root->kind);
+    // printf("%s --%s\n", v_root->size, v_root->kind);
     if (v_root->next != NULL)
     {
-        find_size(v_root->next,start_line,end_line);
+        find_size(v_root->next, start_line, end_line);
     }
 }
 /**
- * @brief aynı satırdaki değişkenleri bulur ve ekler.
+ * @brief Aynı satırdaki değişkenleri bulur ve ekler.
+ * 
  * @param name_var değişken isimlerini alır.
+ * 
  * @param var_line satırlarını alır.
+ * 
  * @param var_kind türlerini alır.
+ * 
  * @param root rootu alır.
  */
 int find_same_line_var(char name_var[50], int var_line, char var_kind[c_Size_s], variable_s *root)
 {
-
     char *token = strtok(name_var, ",");
     while (token != NULL)
     {
@@ -126,7 +127,7 @@ int find_same_line_var(char name_var[50], int var_line, char var_kind[c_Size_s],
         strcat(data.kind, var_kind);
         data.line = var_line;
         // data.id = 0;
-        memset(data.size,NULL,20);
+        memset(data.size, NULL, 20);
         memset(data.name, NULL, strlen(data.name));
         strcat(data.name, token);
         add_variable_data(&data, root);
@@ -136,15 +137,3 @@ int find_same_line_var(char name_var[50], int var_line, char var_kind[c_Size_s],
     return 0;
 }
 
-
-
-
-
-/**
- * @brief  dosyanın içindeki stringi alır.
- * değişken ve fonksiyonları bulur ve struct'a ekler.
- * @param text string dizisi
- *
- *
- *
- */
